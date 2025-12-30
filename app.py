9b45f7c3b5c884c468e8cf313b4716b18b931500
PromptSmithX <hoangtrunghaish1234@gmail.com>
2025-12-30 16:44:08 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import matplotlib as plt
+from ham_tinh import fact
+
+def main():
+    st.title("Phần mềm tính toán giai thừa")
+    number = st.number_input("Nhập số: ",min_value=0, max_value=900)
+
+    if st.button("Enter"):
+        result = fact(number)
+        st.write(f'Kết quả giai thừa của số {number} là {result}')
+main()
